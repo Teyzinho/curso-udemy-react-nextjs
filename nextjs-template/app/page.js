4632ffe130da6c45536ba8b879c1a78292a90319
Thiago <teyextreme@outlook.com>
2023-10-10 10:25:52 -0300

    Next-Template eslint editorconfig prettier

--- nextjs-template/app/page.js
+++ nextjs-template/app/page.js
@@ -1,23 +1,18 @@
-"use client"
-
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
 
 import styled, { css } from 'styled-components';
 
 const Container = styled.div`
-  ${({theme}) => css`
+  ${({ theme }) => css`
     background: ${theme.colors.white};
     color: ${theme.colors.primary};
   `}
-`
+`;
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Container>
-        asd
-      </Container>
+    <main>
+      <Container>asd</Container>
     </main>
-  )
+  );
 }
